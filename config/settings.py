3ca28ab62ccd04e9aedd452600703f02475818c1
Isomiddin <shoxistairiskulova@gmail.com>
2025-03-13 12:00:25 +0300

    ready

--- config/settings.py
+++ config/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
-    'library'
+    'rest_framework',
+    'library',
 ]
 
 MIDDLEWARE = [
